7b31f8f98470ad3497af7737a63b25647e5c9098
HARSH SHARMA <harsh159357@gmail.com>
2018-06-18 13:36:31 +0530

    PHP script for the Reading of all Contacts also its a REST API.

---
+++ phpbackend/contact/ReadContacts.php
@@ -0,0 +1,59 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../config/DatabaseConnection.php';
+include_once '../objects/Contact.php';
+
+// instantiate database and Contact object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$contact = new Contact($db);
+
+// query contacts
+$stmt = $contact->read($db);
+$num = $stmt->rowCount();
+
+// check if more than 0 record found
+if($num>0){
+
+    // contacts array
+    $contacts_arr=array();
+    // $contacts_arr["contacts"]=array();
+
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+
+        $contact_item=array(
+            "_id" => $_id,
+            "name" => $name,
+            "phone" => $phone,
+            "email" => $email,
+            "address" => $address,
+            "latitude" => $latitude,
+            "longitude" => $longitude,
+            "contact_image" => base64_encode($contact_image),
+        );
+
+        array_push($contacts_arr, $contact_item);
+
+        // array_push($contacts_arr["contacts"], $contact_item);
+    }
+    header('HTTP/1.1 200 Contacts Found');
+    echo json_encode($contacts_arr);
+}
+
+else{
+  header('HTTP/1.1 404 No Contacts Found in Remote Database');
+}
+?>
